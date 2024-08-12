63fb3c9ac4eccb396f8145ae0cab1c891706c57a
Alfian noer firdausi <alfiannoerfirdausi2000@gmail.com>
2024-08-12 16:34:31 +0700

    feat/getdata/add-onhtml

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const getData = async(){
+const ul = document.querySelector("#jokes");
+
+const addjoke = async () => {
+  const textjokes = await getData();
+  const newLi = document.createElement("li");
+  newLi.append(textjokes);
+  ul.append(newLi);
+};
+
+const getData = async () => {
+  try {
     const config = {
-        headers :{
-            Accept : 'application/json',
-        }
-    }
-}
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await axios.get("https://icanhazdadjoke.com", config);
+    return res.data.joke;
+  } catch (error) {
+    return "No Jokes Available";
+  }
+};
+
+document.querySelector("button").addEventListener("click", addjoke);
